d690361a14e61bb0f6a6339baa8f619ea353be94
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-03-19 10:58:54 +0100

    Add DB functions for CRUD operations testing

--- server.js
+++ server.js
@@ -12,6 +12,45 @@ mongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUn
   }
   else {
     console.log('Successfully connected to the database');
+    const db = client.db('companyDB');
+
+    db.collection('employees').find({ department: 'IT' }, (err, data) => {
+      console.log('Find all employees from the IT Department - return iteration on Cursor:');
+      if (!err) {
+        data.each((error, employee) => {
+          console.log(employee);
+        })
+      }
+    });
+
+    db.collection('employees').find({ department: 'IT' }).toArray((err, data) => {
+      console.log('Find all employees from the IT Department - return the Array:');
+      if (!err) {
+        console.log(data)
+      }
+    });
+
+    db.collection('employees').findOne({ department: 'IT' }, (err, data) => {
+      console.log('Find all employees from the IT Department - return first matching element:');
+      if (!err) {
+        console.log(data)
+      }
+    });
+
+    db.collection('departments').insertOne({ name: 'Management' }, err => {
+      console.log('Add new item to the Departments collection:');
+      if (err) console.log('err');
+    });
+
+    db.collection('departments').find({}, (err, data) => {
+      console.log('Show all items in Departments collection to check previous operation:');
+      if (!err) {
+        data.each((error, employee) => {
+          console.log(employee);
+        })
+      }
+    });
+
     const app = express();
 
     app.use(cors());
