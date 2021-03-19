7d7043f968b9e51bc1e6468fae762dffb7138cc2
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-03-19 11:34:21 +0100

    Remove CRUD operations testing

--- server.js
+++ server.js
@@ -14,53 +14,6 @@ mongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUn
     console.log('Successfully connected to the database');
     const db = client.db('companyDB');
 
-    db.collection('employees').find({ department: 'IT' }, (err, data) => {
-      console.log('Find all employees from the IT Department - return iteration on Cursor:');
-      if (!err) {
-        data.each((error, employee) => {
-          console.log(employee);
-        })
-      }
-    });
-
-    db.collection('employees').find({ department: 'IT' }).toArray((err, data) => {
-      console.log('Find all employees from the IT Department - return the Array:');
-      if (!err) {
-        console.log(data)
-      }
-    });
-
-    db.collection('employees').findOne({ department: 'IT' }, (err, data) => {
-      console.log('Find all employees from the IT Department - return first matching element:');
-      if (!err) {
-        console.log(data)
-      }
-    });
-
-    db.collection('departments').insertOne({ name: 'Management' }, err => {
-      console.log('Add new item to the Departments collection:');
-      if (err) console.log('err');
-    });
-
-    db.collection('departments').find({}, (err, data) => {
-      console.log('Show all items in Departments collection to check previous operation:');
-      if (!err) {
-        data.each((error, employee) => {
-          console.log(employee);
-        })
-      }
-    });
-
-    db.collection('employees').updateOne({ department: 'IT' }, { $set: { salary: 6000 }}, err => {
-      console.log('Update salary of the first matching employee from the IT Department:')
-      if(err) console.log(err);
-    });
-
-    db.collection('departments').deleteOne({ name: 'Management' }, (err) => {
-      console.log('Remove the Management from the Departmens collection:');
-      if(err) console.log(err);
-    });
-
     const app = express();
 
     app.use(cors());
