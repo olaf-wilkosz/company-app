c3c137b392985e2861c7b046b625f3f52057aadb
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-03-21 00:46:26 +0100

    Add returning the modified or removed document after using PUT and DELETE methods in Departments Routes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -49,12 +49,13 @@ router.post('/departments', async (req, res) => {
 router.put('/departments/:id', async (req, res) => {
   const { name } = req.body;
   try {
-    const dep = await (Department.findById(req.params.id));
-    if (dep) {
-      await Department.updateOne({ _id: req.params.id }, { $set: { name: name } });
-      res.json({ message: 'OK' });
-    }
-    else res.status(404).json({ message: 'Not found...' });
+    await Department.findByIdAndUpdate({ _id: req.params.id }, { $set: { name: name } }, (err, doc) => {
+      if (err) {
+        res.status(404).json({ message: 'Not found...' });
+      } else {
+        res.json(doc);
+      }
+    });
   }
   catch (err) {
     res.status(500).json({ message: err });
@@ -63,12 +64,13 @@ router.put('/departments/:id', async (req, res) => {
 
 router.delete('/departments/:id', async (req, res) => {
   try {
-    const dep = await (Department.findById(req.params.id));
-    if (dep) {
-      await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
-    }
-    else res.status(404).json({ message: 'Not found...' });
+    await Department.findByIdAndDelete({ _id: req.params.id }, (err, doc) => {
+      if (err) {
+        res.status(404).json({ message: 'Not found...' });
+      } else {
+        res.json(doc);
+      }
+    });
   }
   catch (err) {
     res.status(500).json({ message: err });
