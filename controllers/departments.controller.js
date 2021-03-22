8bd02ce6593485b9810336c69753683bbb370da5
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-03-22 21:13:50 +0100

    Move methods from Routes to Controllers

---
+++ controllers/departments.controller.js
@@ -0,0 +1,76 @@
+const Department = require('../models/department.model');
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Department.find());
+  }
+  catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.getRandom = async (req, res) => {
+  try {
+    const count = await Department.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const dep = await Department.findOne().skip(rand);
+    if (!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const dep = await Department.findById(req.params.id);
+    if (!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.addNew = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const newDepartment = new Department({ name: name });
+    await newDepartment.save();
+    res.json({ message: 'OK' });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.edit = async (req, res) => {
+  const { name } = req.body;
+  try {
+    await Department.findByIdAndUpdate({ _id: req.params.id }, { $set: { name: name } }, (err, doc) => {
+      if (err) {
+        res.status(404).json({ message: 'Not found...' });
+      } else {
+        res.json(doc);
+      }
+    });
+  }
+  catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    await Department.findByIdAndDelete({ _id: req.params.id }, (err, doc) => {
+      if (err) {
+        res.status(404).json({ message: 'Not found...' });
+      } else {
+        res.json(doc);
+      }
+    });
+  }
+  catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
